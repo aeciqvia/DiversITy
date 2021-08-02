aebac5a096313a33caa720df40f5aa0804fe3c88
1baldevd <baldev.desai@iqvia.com>
2021-08-02 10:41:13 -0400

    Pushing all changes

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const UploadForm = () => {
     return (
@@ -8,6 +9,20 @@ const UploadForm = () => {
                 <Form.Label>Please select a file to upload.</Form.Label>
                 <Form.Control type="file" />
             </Form.Group>
+
+            <Form.Group>
+                <Form.Label>White, Male</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>White, Female</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">Submit</Button>
         </Form>
     )
 }
