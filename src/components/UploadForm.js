1ceb0ec9c0cbdb07eb4e2489f68596d7eb68f44d
1baldevd <baldev.desai@iqvia.com>
2021-08-02 10:56:23 -0400

    Additional fields

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -22,6 +22,80 @@ const UploadForm = () => {
                 <Form.Text></Form.Text>
             </Form.Group>
 
+            <Form.Group>
+                <Form.Label>Black, Male</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Black, Female</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Asian, Male</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Asian, Female</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Native American, Male</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Native American, Female</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Pacific Islander, Male</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Pacific Islander, Female</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Multiple, Male</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Multiple, Female</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Other, Male</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Other, Female</Form.Label>
+                <Form.Control type="number" />
+                <Form.Text></Form.Text>
+            </Form.Group>
+
+            
+
             <Button variant="primary" type="submit">Submit</Button>
         </Form>
     )
